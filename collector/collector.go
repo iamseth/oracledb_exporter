d9c0e1fbccc5861ff521f9e687d53e20b8207694
Valentin Rusu <valrusu@gmail.com>
2024-07-02 17:25:46 -0400

    Added mutex for access to the err variable written by all metrics collector functions

--- collector/collector.go
+++ collector/collector.go
@@ -241,6 +241,8 @@ func (e *Exporter) scheduledScrape() {
 func (e *Exporter) scrape(ch chan<- prometheus.Metric) {
 	e.totalScrapes.Inc()
 	var err error
+	var errmutex sync.Mutex
+
 	defer func(begun time.Time) {
 		e.duration.Set(time.Since(begun).Seconds())
 		if err == nil {
@@ -313,7 +315,12 @@ func (e *Exporter) scrape(ch chan<- prometheus.Metric) {
 			}
 
 			scrapeStart := time.Now()
-			if err = e.ScrapeMetric(e.db, ch, metric); err != nil {
+			if err1 := e.ScrapeMetric(e.db, ch, metric); err1 != nil {
+				errmutex.Lock()
+				{
+					err = err1
+				}
+				errmutex.Unlock()
 				level.Error(e.logger).Log("scrapeMetricContext", metric.Context, "ScrapeDuration", time.Since(scrapeStart), "msg", err.Error())
 				e.scrapeErrors.WithLabelValues(metric.Context).Inc()
 			} else {
